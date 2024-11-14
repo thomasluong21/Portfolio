cbdf69bb7b2fd5ec0585fc9eb2918148d71f6702
thomas Luong <thomas13t35@gmail.com>
2024-11-13 20:29:49 -0800

    change about profile images

--- javascript/about.js
+++ javascript/about.js
@@ -5,3 +5,20 @@ gsap.from(".about_bg, .container .grid img, .container .grid div", {
     stagger: 0.3,
 });
   
+const mediaQuery = window.matchMedia("(max-height: 700px) and (min-width: 375px)");
+
+function handleViewportChange(e) {
+    const aboutImage = document.querySelector('.about_pic');
+    if (e.matches) {
+        aboutImage.src = "assets/about/about_me_mobile.png";
+    } else {
+        aboutImage.src = "assets/about/about_me.png";
+    }
+}
+
+handleViewportChange(mediaQuery);
+
+mediaQuery.addEventListener("change", handleViewportChange);
+
+
+
--- javascript/footer.js
+++ javascript/footer.js
@@ -2,5 +2,4 @@ document.addEventListener("DOMContentLoaded", function() {
     const currentDateElement = document.querySelector(".current_date");
     const currentYear = new Date().getFullYear();
     currentDateElement.textContent = currentYear;
-    console.log('hello')
 });
